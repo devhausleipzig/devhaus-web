a207f7c27a8b6e548a1de4c53b41758dda5e56c1
mnapearson <marratoon@gmail.com>
2020-12-15 13:31:43 +0100

    add grid layout for work section

--- src/App.js
+++ src/App.js
@@ -78,8 +78,52 @@ export function Academy() {
 
 export function Work() {
   return (
-    <div className="content site-header navbar-padding">
-      <h1>Work</h1>
+    <div className="content">
+      <h1 className="site-header navbar-padding">Work</h1>
+      <section className="grid1">
+        <div className="img1">
+          <img
+            src="https://devopedia.org/images/article/34/1195.1529045532.jpg"
+            alt="img1"
+          ></img>
+        </div>
+        <div className="img2">
+          <img
+            src="https://d35fo82fjcw0y8.cloudfront.net/2019/05/22093043/types-of-mobile-apps-native_incontent1-1024x786.png"
+            alt="img2"
+          ></img>
+        </div>
+        <div className="img3">
+          <img
+            src="https://readdle.com/img/blog/best-calendar-app-iphone-ipad.png?1594316892350"
+            alt="img3"
+          ></img>
+        </div>
+        <div class="cta1">
+          <h2>e.dox</h2>
+          <p>
+            Introducing 212/SE, our most powerful and agile subwoofer, designed
+            exclusively for larger systems and rooms to allow superior state of
+            the art speakers to spring to full voice.
+          </p>
+        </div>
+        <div class="cta2">
+          <h2>fillip</h2>
+          <p>
+            Introducing 212/SE, our most powerful and agile subwoofer, designed
+            exclusively for larger systems and rooms to allow superior state of
+            the art speakers to spring to full voice.
+          </p>
+        </div>
+        <div class="cta3">
+          <h2>tbd</h2>
+          <p>
+            Introducing 212/SE, our most powerful and agile subwoofer, designed
+            exclusively for larger systems and rooms to allow superior state of
+            the art speakers to spring to full voice.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
@@ -167,7 +211,7 @@ export function Contact() {
       </div>
       <div className="image-container">
         <img
-          onMouseOver={changeImage}
+          onClick={changeImage}
           id="contact-image"
           src="https://media.spacebase.com/media/cache/spaces/3691/basislager-4og-k2-1_1280.jpg"
           alt="basislager"
