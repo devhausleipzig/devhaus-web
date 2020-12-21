47b6ae45efc271dce95235a20b1326d05e433b24
mnapearson <marratoon@gmail.com>
2020-12-21 15:57:31 +0100

    add logo with transparent background for academy page

--- src/components/Academy.js
+++ src/components/Academy.js
@@ -4,18 +4,14 @@ export default function Academy() {
   return (
     <div className="content academy">
       <h1 className="site-header navbar-padding">Academy</h1>
-      <div class="academy-apply">
+      <div className="academy-apply">
         <div className="ccl-logo">
-          <img src="/CCLhand.jpeg" alt="ccl-logo"></img>
+          <img src="/Code-Camp-Leipzig-Logo.svg" alt="ccl-logo"></img>
         </div>
         <div className="academy-text">
           <h2 className="apply-link">
             Want to become a professional software developer?
           </h2>
-          <h3>
-            We are currently accepting applications for Code Camp Session 4!
-          </h3>
-
           <button>
             <a href="https://codecampleipzig.de/">Apply now</a>
           </button>
