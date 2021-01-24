cd6e30da5bf5e0437048e26f8b3a253d2cb30573
Gabriel Heinrich <gabriel@codecampleipzig.de>
2021-01-24 20:17:43 +0100

    Fix video

--- src/components/home/HeroVideo.js
+++ src/components/home/HeroVideo.js
@@ -1,11 +1,22 @@
 import "./HeroVideo.scss";
+import fallbackImage from "images/graduates1.jpg";
 
 export default function HeroVideo() {
   return (
-    <div className="hero-video">
-      <video onClick={(event) => event.target.play()} autoPlay loop>
-        <source src="devhaus-720p.webm" type="video/webm"></source>
-      </video>
+    <div
+      className="hero-video"
+      style={{ backgroundImage: `url(${fallbackImage})` }}
+    >
+      <div class="video-background">
+        <iframe
+          src="https://player.vimeo.com/video/504125086?background=1&autoplay=1&loop=1&byline=0&title=0"
+          title="Devhaus Leipzig"
+          frameborder="0"
+          webkitallowfullscreen
+          mozallowfullscreen
+          allowfullscreen
+        ></iframe>
+      </div>
       <div className="hero-title">
         <h1>
           DEV
