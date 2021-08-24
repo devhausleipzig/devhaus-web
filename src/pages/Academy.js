471017804fa86b70758372eaa5afe7fac6aebe47
franzwollang <franzwollang@gmail.com>
2021-08-24 16:17:44 +0200

    Second attempt to fix styling errors

--- src/pages/Academy.js
+++ src/pages/Academy.js
@@ -392,9 +392,7 @@ export default function Academy() {
             <div className="line"></div>
           </div>
           <p>
-              <strong> Due to scheduling constraints for the Winter 2021 Camp, there will be no self-practice/break week. </strong>
-          </p>
-           <p>
+            <strong> Due to scheduling constraints for the Winter 2021 Camp, there will be no self-practice/break week.</strong>
             It's time to refresh and reflect on the material learned so far.
             During this week, there are no classes and no new material. Only
             individual check-in sessions and the opportunity to solidify your
@@ -472,7 +470,7 @@ export default function Academy() {
           including payment plans, government programs, loan partners, and
           scholarships. Make sure to reach out to us, so we can evaluate the
           financing options that fit your specific situation. <br/>
-          Contact us at <strong><a href="mailto:info@devhausleipzig.de">info@devhausleipzig.de</a></strong>
+          Contact us at <strong><a href="mailto:info@devhausleipzig.de">info@devhausleipzig.de</a> </strong>
           with any questions.
         </p>
         <a
